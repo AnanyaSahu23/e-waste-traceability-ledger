ae319dcb386cdebfac640bc567a555ba524c3e51
AnanyaSahu23 <ananya.sahu2024@vitstudent.ac.in>
2026-09-19 12:32:26 +0530

    Added user auth, need to fix organizations and test

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 
 const prisma = require("./utils/prisma");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/api/health", (req, res) => {
   res.json({
