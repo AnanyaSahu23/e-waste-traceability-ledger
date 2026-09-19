1a663856d431bd045919d3e5b2db296a2affafe1
AnanyaSahu23 <ananya.sahu2024@vitstudent.ac.in>
2026-09-19 12:40:02 +0530

    made a change

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
+require("dotenv").config({path: "../.env",});
 const path = require("path");
 
-require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
+// require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 
 if (!process.env.JWT_SECRET || process.env.JWT_SECRET.length < 32) {
   throw new Error("JWT_SECRET must be set to at least 32 characters");
